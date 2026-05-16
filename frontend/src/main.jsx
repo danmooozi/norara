7e593ea9699ab0fe4b27c0828ca8b6a5fa5c7f4f
danmooozi <osiyun0313@gmail.com>
2026-05-16 16:55:04 +0900

    feat: Google Analytics 4 연동 (VITE_GA_ID 환경변수)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './App.css';
 
+const GA_ID = import.meta.env.VITE_GA_ID;
+if (GA_ID) {
+  const s = document.createElement('script');
+  s.async = true;
+  s.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+  document.head.appendChild(s);
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function () { window.dataLayer.push(arguments); };
+  window.gtag('js', new Date());
+  window.gtag('config', GA_ID);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
